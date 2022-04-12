c49d7c8ec8ac0da063e70720647184e27e221e39
Lex Matthews <tanukiguernica@gmail.com>
2022-04-11 22:06:59 -0400

    got api calls working and got it to display necessary info on page (still need 5day)

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,56 @@
+//cors server
+jQuery.ajaxPrefilter(function(options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
+});
 // openWeather api key: 56de1eb517b3c48fb6839f0e3a51dbfb
 var formEl= $("#cityPicker");
 var listContainer= $(".cityDrop");
 var titleContainer= $(".cCityTitle");
 var fivedayContainer= $(".cCity");
 
+var city;
 
-
-
+function init(){
+    
+}
 
 
 function grabLocation(str){
-    var city= str;
-    var lat;
-    var lon;
-    //city.replaceAll(" ","");
-    var url= "//api.openweathermap.org/geo/1.0/direct?q=Orlando,FL,&appid=56de1eb517b3c48fb6839f0e3a51dbfb";
-    fetch(url,{
-        mode: 'cors',
-        headers: {
-          'Access-Control-Allow-Origin':'*'
-        }
-    })
+    city= str;
+    var cUrl= city.replace(" ","");
+    var url= "http://api.openweathermap.org/geo/1.0/direct?q="+ cUrl +",&appid=56de1eb517b3c48fb6839f0e3a51dbfb";
+    fetch(url)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
-        lat= data[0].lat;
-        lon= data[0].lon;
+        var lat= data[0].lat;
+        var lon= data[0].lon;
+        parseCurrent(lat,lon);
+        parseHistoric(lat,lon);
     });
-    console.log(lat, lon);
-    parseCurrent(lat,lon);
-    parseHistoric(lat,lon);
 }
 
 function parseCurrent(x,y){
-
+   var url="https://api.openweathermap.org/data/2.5/onecall?lat="+x+"&lon="+y+"&units=imperial&appid=56de1eb517b3c48fb6839f0e3a51dbfb"; 
+   fetch(url)
+   .then(function(response){
+       return response.json();
+   })
+   .then(function(data){
+        console.log(data);
+        var currObj={
+            name: city,
+            date: makeDate(data.current.dt),
+            temp: data.current.temp,
+            wind: data.current.wind_speed,
+            humidity: data.current.humidity,
+            uvI: data.current.uvi
+        };
+        displayCContent(currObj);
+   });
 }
 
 function parseHistoric(x,y){
@@ -42,14 +58,40 @@ function parseHistoric(x,y){
 }
 
 function makeDate(x){
-
+    var unix = x;
+    var milliseconds = unix * 1000;
+    var dateObject = new Date(milliseconds);
+    var date= dateObject.toLocaleString("en-US", {timeZoneName: "short"});
+    date= date.substring(0,8);
+    return date;
 }
 
-function constructObjs(str1, str2, str3, a, b, c, d){
+function displayCContent(x){
+    console.log(x);
+    var header= document.createElement("h2");
+    header.innerHTML= x.name+" ("+x.date+")";
+    var weatherList= document.createElement("ul");
+    var temperature= document.createElement("li");
+    temperature.innerHTML= x.temp;
+    var windSp= document.createElement("li");
+    windSp.innerHTML= x.wind;
+    var humid= document.createElement("li");
+    humid.innerHTML= x.humidity;
+    var UV= document.createElement("li");
+    UV.innerHTML= x.uvI;
+    titleContainer.append(header);
+    titleContainer.append(weatherList);
+    weatherList.append(temperature);
+    weatherList.append(windSp);
+    weatherList.append(humid);
+    weatherList.append(UV);
 
+    var cButton= document.createElement("button");
+    cButton.innerHTML= x.name;
+    listContainer.append(cButton);
 }
 
-function displayContent(x){
+function displayHContent(x){
 
 }
 
@@ -60,4 +102,5 @@ formEl.on("submit", function(event){
     formEl.children().eq(0).val("");
 });
 
-grabLocation();
+
+init();
