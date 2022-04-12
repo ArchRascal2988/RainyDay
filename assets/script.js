2596519a5abeb76c2274da8ec849110689f33edb
Lex Matthews <tanukiguernica@gmail.com>
2022-04-11 22:51:14 -0400

    finished js, still need to fill out css fully

--- assets/script.js
+++ assets/script.js
@@ -18,6 +18,7 @@ function init(){
 
 
 function grabLocation(str){
+    clearContent();
     city= str;
     var cUrl= city.replace(" ","");
     var url= "http://api.openweathermap.org/geo/1.0/direct?q="+ cUrl +",&appid=56de1eb517b3c48fb6839f0e3a51dbfb";
@@ -29,7 +30,6 @@ function grabLocation(str){
         var lat= data[0].lat;
         var lon= data[0].lon;
         parseCurrent(lat,lon);
-        parseHistoric(lat,lon);
     });
 }
 
@@ -49,12 +49,31 @@ function parseCurrent(x,y){
             humidity: data.current.humidity,
             uvI: data.current.uvi
         };
+        console.log(data.current.dt);
+        parseHistoric(x,y, data.current.dt);
         displayCContent(currObj);
    });
 }
 
-function parseHistoric(x,y){
-
+function parseHistoric(x,y,z){
+    for(i=0;i<5; i++){
+        z-= 86400;
+        var url="https://api.openweathermap.org/data/2.5/onecall/timemachine?lat="+x+"&lon="+y+"&dt="+ z +"&units=imperial&appid=56de1eb517b3c48fb6839f0e3a51dbfb"; 
+        fetch(url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+             console.log(data);
+             var hObj={
+                date: makeDate(data.current.dt),
+                temp: data.current.temp,
+                wind: data.current.wind_speed,
+                humidity: data.current.humidity
+            };
+        displayHContent(hObj);
+        });
+    }
 }
 
 function makeDate(x){
@@ -72,11 +91,11 @@ function displayCContent(x){
     header.innerHTML= x.name+" ("+x.date+")";
     var weatherList= document.createElement("ul");
     var temperature= document.createElement("li");
-    temperature.innerHTML= x.temp;
+    temperature.innerHTML= x.temp+" °F";
     var windSp= document.createElement("li");
-    windSp.innerHTML= x.wind;
+    windSp.innerHTML= x.wind+" MPH";
     var humid= document.createElement("li");
-    humid.innerHTML= x.humidity;
+    humid.innerHTML= x.humidity+" %";
     var UV= document.createElement("li");
     UV.innerHTML= x.uvI;
     titleContainer.append(header);
@@ -88,11 +107,27 @@ function displayCContent(x){
 
     var cButton= document.createElement("button");
     cButton.innerHTML= x.name;
+    cButton.setAttribute("id", "cityBtn");
     listContainer.append(cButton);
 }
 
 function displayHContent(x){
-
+    var card= document.createElement("div");
+    var header= document.createElement("h2");
+    header.innerHTML= "("+x.date+")";
+    var weatherList= document.createElement("ul");
+    var temperature= document.createElement("li");
+    temperature.innerHTML= x.temp+" °F";
+    var windSp= document.createElement("li");
+    windSp.innerHTML= x.wind+" MPH";
+    var humid= document.createElement("li");
+    humid.innerHTML= x.humidity+" %";
+    fivedayContainer.append(card);
+    card.append(header);
+    card.append(weatherList);
+    weatherList.append(temperature);
+    weatherList.append(windSp);
+    weatherList.append(humid);
 }
 
 formEl.on("submit", function(event){
@@ -102,5 +137,15 @@ formEl.on("submit", function(event){
     formEl.children().eq(0).val("");
 });
 
+$("#cityBtn").on("click", function(event){
+    var city= event.target.text();
+    console.log(event.target.text());
+    grabLocation(city);
+}); 
+
+function clearContent(){
+    titleContainer.empty();
+    fivedayContainer.empty();
+}
 
 init();
